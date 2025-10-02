c6070b35c4d6f4f1b42294b36c664694d1b54673
jafar <jafar_magomedov0594@mail.ru>
2025-10-02 15:09:38 +0300

    Подправил nginx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import { useCallback } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  async function login(email: string, password: string) {
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      // если нужны cookie-сессии:
+      // credentials: 'include',
+      body: JSON.stringify({ email, password }),
+    });
+  
+    if (!response.ok) {
+      const errText = await response.text().catch(() => '');
+      throw new Error(`Login failed: ${response.status} ${errText}`);
+    }
+  
+    return response.json();
+  }
+  
+  async function getProfile() {
+    const response = await fetch('/api/users/me', {
+      method: 'GET',
+      // credentials: 'include',
+    });
+  
+    if (!response.ok) throw new Error('Failed to load profile');
+    return response.json();
+  }
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeEmail = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  }, [])
+
+  const onChangePassword = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  }, [])
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="email" placeholder="Email" onChange={onChangeEmail}/>
+        <input type="password" placeholder="Password"  onChange={onChangePassword}/>
+        <button onClick={() => login(email, password)}>Login</button>
+        <button onClick={() => getProfile()}>Get Profile</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
