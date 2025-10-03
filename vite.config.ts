a95a3d233db770dd88bc5492e31bfc4a7d342d5a
jafar <jafar_magomedov0594@mail.ru>
2025-10-03 13:42:19 +0300

    proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,7 +10,7 @@ export default defineConfig({
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path.replace(/^\/api\//, '/'),
-      },
-    },
-  },
+      }
+    }
+  }
 })
