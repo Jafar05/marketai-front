b1c5846361f56967c899ed92589800b73c525f9b
jafar <jafar_magomedov0594@mail.ru>
2025-10-08 18:44:40 +0300

    React

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,34 @@
-import React from "react";
-import { Typography, Button, Space, Card } from "antd";
+import React, {useEffect, useState} from "react";
+import {Typography, Button, Space, Card, message} from "antd";
 import { RocketOutlined } from "@ant-design/icons";
-import { useNavigate } from "react-router-dom";
+import {useNavigate, useSearchParams} from "react-router-dom";
 import styles from "./Main.module.css";
 
 const { Title, Paragraph } = Typography;
 
 const Main: React.FC = () => {
     const navigate = useNavigate();
+    const [params] = useSearchParams();
+
+    const [messageApi, contextHolder] = message.useMessage();
+
+
+    useEffect(() => {
+        const token = params.get("token")
+        fetch(`/api/auth/api/v1/verify-email?token=${token}`)
+            .then(res => res.json())
+            .then(() => messageApi.open({
+                type: 'success',
+                content: "Email успешно подтверждён!",
+            }))
+            .catch(() => messageApi.open({
+                type: 'error',
+                content: "Ошибка при подтверждении email",
+            }));
+    }, [params]);
+
+
+
 
     const handleStart = () => {
         navigate("/login");
@@ -15,6 +36,7 @@ const Main: React.FC = () => {
 
     return (
         <div className={styles.wrapper}>
+            {contextHolder}
             <Card className={styles.card}>
                 <Space direction="vertical" size="large" style={{ width: "100%" }}>
                     <RocketOutlined className={styles.icon} />
